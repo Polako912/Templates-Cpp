43357b362745053c269933003517a82525d37a04
Polako912 <michal.polak37@gmail.com>
2019-04-11 13:20:00 +0200

    added add and sub functions

--- Templates/Main.cpp
+++ Templates/Main.cpp
@@ -12,23 +12,42 @@ const T& Max(const T& a, const T& b)
   return a > b ? a : b;
 }
 
+template <class T>
+const T& Add(const T& a, const T& b)
+{
+  return a + b;
+}
+
+template <class T>
+const T& Sub(const T& a, const T& b)
+{
+  return a - b;
+}
+
 int main ()
 {
   int x = 10; 
   int y = 15;
-  double z = 100;
+  double z;
   double w = 99;
+
+  std::cin >> z;
 
   int MinInt = Min<int>(x,y);
   double MinDouble = Min<double>(z, w);
   int MaxInt = Max<int>(x, y);
   double MaxDouble = Max<double>(z, w);
+  int sum = Add<int>(x, y);
+  double dif = Sub<double>(z, w);
 
   std::cout << "Min int: " << MinInt << std::endl;
   std::cout << "Min double: " << MinDouble << std::endl;
   std::cout << "Max int: " << MaxInt << std::endl;
   std::cout << "Max double: " << MaxDouble << std::endl;
+  std::cout << "Sum: " << sum << std::endl;
+  std::cout << "Difference: " << dif << std::endl;
 
+  std::cin.get();
   std::cin.get();
 
 	return 0;
